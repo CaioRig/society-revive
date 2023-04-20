c77abef8600b0e7e53a8085f58f8423d8f7de02d
CaioRig <caiorigotto@gmail.com>
2023-04-20 14:41:53 -0300

    made the whole card clickable

--- idle-game-noname-front/src/components/Actions/Cards.js
+++ idle-game-noname-front/src/components/Actions/Cards.js
@@ -34,38 +34,43 @@ const ActionCard = ({
                 maxWidth: "250px",
                 margin: "10px",
                 borderRadius: "10px",
-                animation: `${fadeIn} 0.5s ease-in`,
+                animation: `${fadeIn} 1s ease-in`,
                 display: "flex",
                 flexDirection: "column",
                 justifyContent: "space-between",
                 height: "100%",
             }}
         >
-            <CardContent>
-                <Box>
-                    <Typography variant="h6" title={qtyTitle}>
-                        {actionNumberIcon}
-                        {actionQty}
-                    </Typography>
+            <Box
+                onClick={isActionDisabled ? null : actionOnClick}
+                style={{ cursor: isActionDisabled ? "default" : "pointer" }}
+            >
+                <CardContent>
+                    <Box>
+                        <Typography variant="h6" title={qtyTitle}>
+                            {actionNumberIcon}
+                            {actionQty}
+                        </Typography>
+                        <Typography
+                            variant="body2"
+                            color={isActionDisabled ? "error" : "primary"}
+                        >
+                            Cost: {actionPrice} {actionStockName}
+                        </Typography>
+                    </Box>
                     <Typography
-                        variant="body2"
-                        color={isActionDisabled ? "error" : "primary"}
+                        variant="body1"
+                        sx={{
+                            mt: 1,
+                            mb: 2,
+                            whiteSpace: "pre-line",
+                        }}
+                        title={isActionDisabled ? disabledTitle : enabledTitle}
                     >
-                        Cost: {actionPrice} {actionStockName}
+                        {isActionDisabled ? disabledTitle : enabledTitle}
                     </Typography>
-                </Box>
-                <Typography
-                    variant="body1"
-                    sx={{
-                        mt: 1,
-                        mb: 2,
-                        whiteSpace: "pre-line",
-                    }}
-                    title={isActionDisabled ? disabledTitle : enabledTitle}
-                >
-                    {isActionDisabled ? disabledTitle : enabledTitle}
-                </Typography>
-            </CardContent>
+                </CardContent>
+            </Box>
             <Box>
                 <Button
                     onClick={actionOnClick}
