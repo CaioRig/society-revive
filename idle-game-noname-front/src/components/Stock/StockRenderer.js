3830f2d5154c31c932748e6911b0f0417d545419
CaioRig <caiorigotto@gmail.com>
2023-04-20 15:50:12 -0300

    deleted Resources.js file, as it was not necessary anymore

--- idle-game-noname-front/src/components/Stock/StockRenderer.js
+++ idle-game-noname-front/src/components/Stock/StockRenderer.js
@@ -1,9 +1,10 @@
-import React, { useContext } from "react";
-import GlobalStateContext from "../../global/GlobalStateContext";
-import StockCard from "./StockCard";
 import ForestIcon from '@mui/icons-material/Forest';
 import ConstructionIcon from '@mui/icons-material/Construction';
 import HandymanIcon from '@mui/icons-material/Handyman';
+
+import React, { useContext } from "react";
+import GlobalStateContext from "../../global/GlobalStateContext";
+import StockCard from "./StockCard";
 
 const StockRenderer = () => {
     const { Stock, PerSec } = useContext(GlobalStateContext);
@@ -16,12 +17,17 @@ const StockRenderer = () => {
                 Name={"Resources"}
                 Img={<ForestIcon />}
             />
-            <StockCard
-                Stock={Stock.buildingMaterialNumber}
-                PerSec={PerSec.buildingMaterialPerSec}
-                Name={"Building Materials"}
-                Img={<ConstructionIcon />}
-            />
+
+            {Stock.buildingMaterialNumber < 0 ?
+                <></>
+                :
+                <StockCard
+                    Stock={Stock.buildingMaterialNumber}
+                    PerSec={PerSec.buildingMaterialPerSec}
+                    Name={"Building Materials"}
+                    Img={<ConstructionIcon />}
+                />
+            }
             <StockCard
                 Stock={Stock.craftingMaterialNumber}
                 PerSec={PerSec.craftingMaterialPerSec}
